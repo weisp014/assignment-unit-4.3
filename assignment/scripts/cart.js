0c8d9998d0b3096dd471cc49538b53e5dc96ded4
Ryan Weispfenning <weisp014@gmail.com>
2023-02-12 13:47:26 -0600

    created listItems and empty function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,12 +3,30 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 
-function addItem (item) {
+function addItem (item) {   //function adds items to an array
     basket.push(item);
     console.log("adding item:", item);
-    console.log("updated cart", basket);
+    console.log("updated cart", basket);    //output entire basket contents
     return true;
 }
 
 addItem("bananas");
 addItem("coffee");
+
+function listItems (array) {
+    console.log("list out entire basket array", basket);
+    for (let item of array) {
+        console.log(item);  //log each item individually in the array
+    }
+    return true;
+}
+
+listItems(basket);
+
+function empty (array) {    //function clears items from array
+    array.length = 0;     //clear contents of the array
+    return array;
+}
+
+empty(basket);
+console.log("Basket should be empty now:",basket);
