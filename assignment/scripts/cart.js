67668d83b96110d00cf186dd7ddf79a0ec6c8d4e
Ryan Weispfenning <weisp014@gmail.com>
2023-02-12 13:25:03 -0600

    created addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,14 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem (item) {
+    basket.push(item);
+    console.log("adding item:", item);
+    console.log("updated cart", basket);
+    return true;
+}
+
+addItem("bananas");
+addItem("coffee");
